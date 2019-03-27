d70c2efb4999420690fa5c70aba157c54d3eebfe
you <l>
2019-03-27 22:08:24 +0800

    增加发送邮件功能

--- runtest.py
+++ runtest.py
@@ -1,17 +1,47 @@
-import unittest, time
+import unittest, time, smtplib, os
 from test_case import test_baidu, test_youdao
 from HTMLTestRunner import HTMLTestRunner
+from email.mime.text import MIMEText
+from email.header import Header
 
-# 指定测试用例为当前文件夹下的test_case目录
-test_dir = './test_case'
+# 定义发送邮件
+def send_main(file_new):
+    f = open(file_new, 'rb')
+    mail_body = f.read()
 
-discover = unittest.defaultTestLoader.discover(test_dir,
-                                               pattern='test_*.py')
+    msg = MIMEText(mail_body, 'html', 'utf-8')
+    msg['Subject'] = Header("自动化测试报告", 'utf-8')
+
+    smtp = smtplib.SMTP()
+    # 连接发送邮箱服务器
+    smtp.connect("smtp.qq.com")
+    # 登录发送邮箱
+    smtp.login("username@qq.com", "SMTP授权码") #qancfhuunyuoihbe
+    # 发送邮件（发送邮箱和接收邮箱地址）
+    smtp.sendmail("username@qq.com", "receiver@163.com",
+                  msg.as_string())
+    smtp.quit()
+    print('email has send out!')
+
+# 查找测试报告目录，找到最新生成的测试报告文件
+def new_report(testreport):
+    lists = os.listdir(testreport)
+    lists.sort(key=lambda fn:os.path.getmtime(testreport + "\\" + fn))
+    file_new = os.path.join(testreport, lists[-1])
+    print(file_new)
+    return file_new
 
 if __name__ == '__main__':
 
-    now = time.strftime("%y-%m-%d %H_%M_%S")
-    filename = test_dir + '/' + now + 'result.html'
+    # 测试用例和测试报告路径
+    test_dir = "./test_case"
+    test_report = "./report"
+
+    discover = unittest.defaultTestLoader.discover(test_dir,
+                                                   pattern='test_*.py')
+    # 生成测试html报告
+    now = time.strftime("%y-%m-%d_%H_%M_%S")
+    filename = test_report + '/' + now + 'result.html'
     fp = open(filename, 'wb')
     runner = HTMLTestRunner(stream=fp,
                             title='测试报告',
@@ -19,7 +49,16 @@ if __name__ == '__main__':
     runner.run(discover)
     fp.close()
 
+    # 获取最新测试报告
+    new_report = new_report(test_report)
+    # 发送最新测试报告邮件
+    send_main(new_report)
+
+
+
+
 """
+# 通过discover执行目录下所有的用例
 if __name__ == "__main__":
     discover = unittest.defaultTestLoader.discover("./test_case/",
                                                    pattern="test_*.py")
@@ -29,6 +68,7 @@ if __name__ == "__main__":
 """
 
 """
+# 通过测试套件suite指定执行某个用例（或按顺序执行）
 #discover = unittest.defaultTestLoader.discover("./test_case/",
 #pattern="test_*.py")
 suite = unittest.TestSuite()
